5fad772a1eb99b20aba15364fee0110f37f90ebb
omr <omr@DESKTOP-HK1QTE6>
2016-12-07 13:05:18 -0800

    /

--- AutoComplete.Core/TrieSerializer.cs
+++ AutoComplete.Core/TrieSerializer.cs
@@ -11,7 +11,7 @@ namespace AutoComplete.Core
 {
     internal class TrieSerializer
     {
-        #region Serialzie
+        #region Serializer
 
         public static void SerializeHeaderWithJsonSerializer(Stream header, TrieIndexHeader trieIndexHeader)
         {
