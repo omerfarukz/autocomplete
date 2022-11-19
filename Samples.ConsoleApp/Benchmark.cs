364da4aae3f1bd37cb5dca4958046af5185080d7
omerfarukz <ulrozremo@gmail.com>
2022-11-20 01:11:37 +0300

    Fix version

---
+++ Samples.ConsoleApp/Benchmark.cs
@@ -0,0 +1,39 @@
+using AutoComplete.Domain;
+using BenchmarkDotNet.Attributes;
+
+namespace Samples.ConsoleApp;
+
+public class Benchmark
+{
+    private readonly Sample _sample = new Sample("header.bin", "index.bin", "tail.bin");
+    
+    [GlobalSetup]
+    public async Task Build()
+    {
+        await _sample.Build();
+    }
+    
+    [Benchmark]
+    public SearchResult search_door_get_one_item()
+    {
+        return _sample.Search("door", 1, false);
+    }
+
+    [Benchmark]
+    public SearchResult search_door_get_five_items()
+    {
+        return _sample.Search("door", 5, false);
+    }
+    
+    [Benchmark]
+    public SearchResult search_car_get_one_item()
+    {
+        return _sample.Search("car", 1, false);
+    }
+
+    [Benchmark]
+    public SearchResult search_car_get_five_items()
+    {
+        return _sample.Search("car", 5, false);
+    }
+}
--- Samples.ConsoleApp/Program.cs
+++ Samples.ConsoleApp/Program.cs
@@ -1,41 +1,4 @@
-﻿using AutoComplete.Domain;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 using Samples.ConsoleApp;
 
 BenchmarkRunner.Run(typeof(Benchmark));
-
-public class Benchmark
-{
-    private readonly Sample _sample = new Sample("header.bin", "index.bin", "tail.bin");
-    
-    [GlobalSetup]
-    public async void Build()
-    {
-        await _sample.Build();
-    }
-    
-    [Benchmark]
-    public SearchResult search_door_get_one_item()
-    {
-        return _sample.Search("door", 1, false);
-    }
-
-    [Benchmark]
-    public SearchResult search_door_get_five_items()
-    {
-        return _sample.Search("door", 5, false);
-    }
-    
-    [Benchmark]
-    public SearchResult search_car_get_one_item()
-    {
-        return _sample.Search("car", 1, false);
-    }
-
-    [Benchmark]
-    public SearchResult search_car_get_five_items()
-    {
-        return _sample.Search("car", 5, false);
-    }
-}
